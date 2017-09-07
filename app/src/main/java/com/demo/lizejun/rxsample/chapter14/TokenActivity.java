8dafaebd2f5368fc4cb58f834a1177f4c8ffbda5
李泽骏 <lizejun@meizu.com>
2017-09-07 12:48:33 +0800

    [2017.09.07]修改第十四章例子

--- app/src/main/java/com/demo/lizejun/rxsample/chapter14/TokenActivity.java
+++ app/src/main/java/com/demo/lizejun/rxsample/chapter14/TokenActivity.java
@@ -1,6 +1,5 @@
 package com.demo.lizejun.rxsample.chapter14;
 
-import android.content.pm.ProviderInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,17 +9,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.demo.lizejun.rxsample.R;
-import com.demo.lizejun.rxsample.utils.Store;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import java.util.concurrent.Callable;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -31,14 +35,16 @@ public class TokenActivity extends AppCompatActivity {
     private static final String ERROR_TOKEN = "error_token";
     private static final String ERROR_RETRY = "error_retry";
 
-    private Button mBtnRequest;
+    private Button mBtnRequestObservable;
+    private Button mBtnRequestFlow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_token);
-        mBtnRequest = (Button) findViewById(R.id.bt_request);
-        mBtnRequest.setOnClickListener(new View.OnClickListener() {
+        mBtnRequestObservable = (Button) findViewById(R.id.bt_request_observable);
+        mBtnRequestObservable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startRequest(0);
@@ -48,6 +54,19 @@ public class TokenActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 startRequest(1);
+            }
+        });
+        mBtnRequestFlow = (Button) findViewById(R.id.bt_request_flow);
+        mBtnRequestFlow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFlowRequest(0);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startFlowRequest(1);
             }
         });
     }
@@ -66,13 +85,10 @@ public class TokenActivity extends AppCompatActivity {
                 return getUserObservable(index, token);
             }
         }).retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
-
             private int mRetryCount = 0;
-
             @Override
             public ObservableSource<?> apply(Observable<Throwable> throwableObservable) throws Exception {
                 return throwableObservable.flatMap(new Function<Throwable, ObservableSource<?>>() {
-
                     @Override
                     public ObservableSource<?> apply(Throwable throwable) throws Exception {
                         Log.d(TAG, index + ":" + "发生错误=" + throwable + ",重试次数=" + mRetryCount);
@@ -122,5 +138,80 @@ public class TokenActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void startFlowRequest(final int index) {
+        Flowable<String> flow = Flowable.defer(new Callable<Publisher<? extends String>>() {
+            @Override
+            public Publisher<? extends String> call() throws Exception {
+                String cacheToken = TokenLoader.getInstance().getCacheToken();
+                Log.d(TAG, index + "获取到缓存Token=" + cacheToken);
+                return Flowable.just(cacheToken);
+            }
+        }).flatMap(new Function<String, Publisher<String>>() {
+            @Override
+            public Publisher<String> apply(String token) throws Exception {
+                return getUserFlow(index, token);
+            }
+        }).retryWhen(new Function<Flowable<Throwable>, Publisher<?>>() {
+            private int mRetryCount = 0;
+            @Override
+            public Publisher<?> apply(Flowable<Throwable> throwableFlow) throws Exception {
+                return throwableFlow.flatMap(new Function<Throwable, Publisher<?>>() {
+                    @Override
+                    public Publisher<?> apply(Throwable throwable) throws Exception {
+                        Log.d(TAG, index + ":" + "发生错误=" + throwable + ",重试次数=" + mRetryCount);
+                        if (mRetryCount > 0) {
+                            return Flowable.error(new Throwable(ERROR_RETRY));
+                        } else if (ERROR_TOKEN.equals(throwable.getMessage())) {
+                            mRetryCount++;
+                            return TokenLoader.getInstance().getNetTokenFlowLocked();
+                        } else {
+                            return Flowable.error(throwable);
+                        }
+                    }
+                });
+            }
+        });
+        Subscriber<String> subscriber = new Subscriber<String>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                //由于运行在不同的线程，因此需要手动调用一次request。
+                s.request(1);
+            }
+
+            @Override
+            public void onNext(String value) {
+                Log.d(TAG, index + ":" + "收到信息=" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, index + ":" + "onError=" + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, index + ":" + "onComplete");
+            }
+        };
+        flow.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(subscriber);
+    }
+
+    private Flowable<String> getUserFlow(final int index, final String token) {
+        return Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> e) throws Exception {
+                Log.d(TAG, index + "使用token=" + token + "发起请求" + ",request=" + e.requested());
+                //模拟根据Token去请求信息的过程。
+                long tokeTime = Long.valueOf(token);
+                if (System.currentTimeMillis() - tokeTime < 2000) {
+                    e.onNext(index + ":" + token + "的用户信息");
+                } else {
+                    e.onError(new Throwable(ERROR_TOKEN));
+                }
+            }
+        }, BackpressureStrategy.ERROR);
     }
 }
--- app/src/main/java/com/demo/lizejun/rxsample/chapter14/TokenLoader.java
+++ app/src/main/java/com/demo/lizejun/rxsample/chapter14/TokenLoader.java
@@ -5,12 +5,15 @@ import com.demo.lizejun.rxsample.utils.Store;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.functions.Consumer;
-import io.reactivex.internal.observers.BlockingObserver;
-import io.reactivex.internal.operators.flowable.BlockingFlowableNext;
+import io.reactivex.processors.PublishProcessor;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 
@@ -21,6 +24,8 @@ public class TokenLoader {
     private AtomicBoolean mRefreshing = new AtomicBoolean(false);
     private PublishSubject<String> mPublishSubject;
     private Observable<String> mTokenObservable;
+    private PublishProcessor<String> mPublishProcessor;
+    private Flowable<String> mTokenFlow;
 
     private TokenLoader() {
         mPublishSubject = PublishSubject.create();
@@ -32,6 +37,27 @@ public class TokenLoader {
                 e.onNext(String.valueOf(System.currentTimeMillis()));
             }
         }).doOnNext(new Consumer<String>() {
+            @Override
+            public void accept(String token) throws Exception {
+                Log.d(TAG, "存储Token=" + token);
+                Store.getInstance().setToken(token);
+                mRefreshing.set(false);
+            }
+        }).doOnError(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                mRefreshing.set(false);
+            }
+        }).subscribeOn(Schedulers.io()).serialize();
+        mPublishProcessor = PublishProcessor.create();
+        mTokenFlow = Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> e) throws Exception {
+                Thread.sleep(1000);
+                Log.d(TAG, "发送Token");
+                e.onNext(String.valueOf(System.currentTimeMillis()));
+            }
+        }, BackpressureStrategy.BUFFER).doOnNext(new Consumer<String>() {
             @Override
             public void accept(String token) throws Exception {
                 Log.d(TAG, "存储Token=" + token);
@@ -70,6 +96,20 @@ public class TokenLoader {
 
     private void startTokenRequest() {
         mTokenObservable.subscribe(mPublishSubject);
+    }
+
+    public Flowable<String> getNetTokenFlowLocked() {
+        if (mRefreshing.compareAndSet(false, true)) {
+            Log.d(TAG, "没有请求，发起一次新的Token请求");
+            startTokenFlowRequest();
+        } else {
+            Log.d(TAG, "已经有请求，直接返回等待");
+        }
+        return mPublishProcessor;
+    }
+
+    private void startTokenFlowRequest() {
+        mTokenFlow.subscribe(mPublishProcessor);
     }
 
 }
